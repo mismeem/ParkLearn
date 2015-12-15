a388f1ac3332d34912f6ce822b5bef4708469caf
mismeem <mimiscott94@yahoo.com>
2015-12-14 23:33:42 -0500

    finished

--- app/src/main/java/com/mimi/parklearn/MainActivity.java
+++ app/src/main/java/com/mimi/parklearn/MainActivity.java
@@ -4,19 +4,46 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button button2;
 
+    /*
+    * This method creates the startup window
+    * it adds in a button which will lead to the Park list activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        assert savedInstanceState == null;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button2 = (Button) findViewById(R.id.button2);
+
+        assert repOK();
     }
 
+    //when button is clicked, opens ParkListActivity
     public void openParkList(View view){
         Intent intent = new Intent(this, ParkListActivity.class);
         startActivity(intent);
+
+        openedtest();
+
     }
+
+    private void openedtest(){
+        if (BuildConfig.DEBUG) {
+            Log.d("open", "opened ParkListActivity");
+        }
+    }
+
+    private boolean repOK(){
+        return button2.isClickable();
+    }
+
 }
