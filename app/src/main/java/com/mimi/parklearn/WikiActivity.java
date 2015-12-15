4528b809cc925d1643ed20203e669580d622aaeb
mismeem <mimiscott94@yahoo.com>
2015-12-14 23:34:26 -0500

    finished

--- app/src/main/java/com/mimi/parklearn/WikiActivity.java
+++ app/src/main/java/com/mimi/parklearn/WikiActivity.java
@@ -17,95 +17,24 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
+/*
+ * gets the intent from the last activity which contains the appropriate park name string
+ * opens a webview of the corresponding wikipedia page
+ */
 public class WikiActivity extends AppCompatActivity {
     Button clicked;
     String ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        assert savedInstanceState != null;
+
         super.onCreate(savedInstanceState);
         WebView webview = new WebView(this);
         setContentView(webview);
         Intent intent = getIntent();
         String ID = intent.getStringExtra("id");
-        Log.v("wiki id", ID);
 
-/*
-        if(ID.equals(R.string.button_acadia)){
-            clicked = (Button) findViewById(R.id.button_acadia);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_arches)){
-            clicked = (Button) findViewById(R.id.button_arches);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_bigbend)){
-            clicked = (Button) findViewById(R.id.button_bigbend);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_craterlake)){
-            clicked = (Button) findViewById(R.id.button_craterlake);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_deathvalley)){
-            clicked = (Button) findViewById(R.id.button_deathvalley);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_denali)){
-            clicked = (Button) findViewById(R.id.button_denali);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_everglades)){
-            clicked = (Button) findViewById(R.id.button_everglades);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_glacier)){
-            clicked = (Button) findViewById(R.id.button_glacier);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_grandcanyon)){
-            clicked = (Button) findViewById(R.id.button_grandcanyon);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_hawaii)){
-            clicked = (Button) findViewById(R.id.button_hawaii);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_hotsprings)){
-            clicked = (Button) findViewById(R.id.button_hotsprings);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_joshuatree)){
-            clicked = (Button) findViewById(R.id.button_joshuatree);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_mesaverde)){
-            clicked = (Button) findViewById(R.id.button_mesaverde);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_olympic)){
-            clicked = (Button) findViewById(R.id.button_olympic);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_petrifiedforest)){
-            clicked = (Button) findViewById(R.id.button_petrifiedforest);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_rainier)){
-            clicked = (Button) findViewById(R.id.button_rainier);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_roosevelt)){
-            clicked = (Button) findViewById(R.id.button_roosevelt);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_RMNP)){
-            clicked = (Button) findViewById(R.id.button_RMNP);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_sequoia)){
-            clicked = (Button) findViewById(R.id.button_sequoia);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_shenandoah)){
-            clicked = (Button) findViewById(R.id.button_shenandoah);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_smokymountains)){
-            clicked = (Button) findViewById(R.id.button_smokymountains);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_windcave)){
-            clicked = (Button) findViewById(R.id.button_windcave);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_yellowstone)){
-            clicked = (Button) findViewById(R.id.button_yellowstone);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_yosemite)){
-            clicked = (Button) findViewById(R.id.button_yosemite);
-        }
-        else if(intent.getStringExtra("id").equals(R.id.button_zion)){
-            clicked = (Button) findViewById(R.id.button_zion);
-        }*/
-        //String name = (String) clicked.getText();
+        Log.d("wiki id", ID);
 
         String forlink = "";
         for (int i = 0; i < ID.length(); i++) {
@@ -123,6 +52,14 @@ public class WikiActivity extends AppCompatActivity {
         startActivity(intent1);
         webview.loadUrl(url + forlink);
 
+        checkOpenedWikipedia(url, ID);
+
+    }
+
+    private void checkOpenedWikipedia(String url, String ID){
+        if(BuildConfig.DEBUG){
+            Log.d("wiki url", url+ID);
+        }
     }
 
 }
